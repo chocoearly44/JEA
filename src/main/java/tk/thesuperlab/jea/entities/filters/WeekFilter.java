df8f4a6d62b2ac9ad0cf44f4ad2d496fb01d216c
chocoearly44 <chocoearly44@gmail.com>
2022-02-20 11:40:54 +0100

    Popravljeno tedensko filtriranje urnika

--- src/main/java/tk/thesuperlab/jea/entities/filters/WeekFilter.java
+++ src/main/java/tk/thesuperlab/jea/entities/filters/WeekFilter.java
@@ -1,7 +1,24 @@
-package tk.thesuperlab.jea.entities;
+package tk.thesuperlab.jea.entities.filters;
 
-public enum TimetableFilter {
+/**
+ * Tedenski filter za urnik
+ *
+ * @author chocoearly44
+ * @since 2.1
+ */
+public enum WeekFilter {
+	/**
+	 * Pridobi urnik za prejšnji teden
+	 */
 	LAST_WEEK,
+
+	/**
+	 * Pridobi urnik za trenutni (tekoči) teden
+	 */
 	CURRENT_WEEK,
+
+	/**
+	 * Pridobi urnik za naslednji teden
+	 */
 	NEXT_WEEK
 }
